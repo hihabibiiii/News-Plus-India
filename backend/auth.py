657f08af3dc97bd9b3a5dfa814e495ac4c0e2933
Habib <hshabibullah@gmail.com>
2026-02-21 14:04:59 +0530

    Fix password Issue

--- backend/auth.py
+++ backend/auth.py
@@ -5,7 +5,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # pre-generated hash for password: admin123
 ADMIN = {
     "username": "admin",
-    "password": "$2b$12$KIXQ4QbZ8H6z0x9QxZ8H6uQ0GQm1l0p0WQ9v1pQJ7sY7QJ0zQJ7Q2"
+    "password": "$2b$12$xtu58A0nS.2wYdsIGnfY7OzV06c.aJjo.RIjcLBXydgaMNTIxJYQS"
 }
 
 def verify_password(plain_password, hashed_password):
